2060e823641dececcb41dec59f65244fd05cce1f
l.garciago.2017 <lucia_garcia_gonzalez@hotmail.es>
2018-03-23 16:28:27 +0100

    WORK

--- openfda1/show_repos_new.py
+++ openfda1/show_repos_new.py
@@ -4,17 +4,28 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 #I have change the api of the teacher by my api
-conn = http.client.HTTPSConnection("api.github.com")
-conn.request("GET", "/orgs/elastic/repos", None, headers)
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
 conn.close()
 
 repos = json.loads(repos_raw)
-repo = repos[0]
-x = 0
-while x < 30:
-    print("The owner of the...")                                                                                     )
-print("Total number of repos is", len(repos))                             
-print("The owner of the first repository is", repo['owner']['login'])
+print("The id is", repos['results'][0]['id'])
+print("The purpose is", repos['results'][0]['purpose'])
+print("The name of the manufacturer is", repos['results'][0]['openfda']['manufacturer_name'])
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?limit=10", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+i = 0
+while i < 10:
+    print("The id is", repos['results'][i]['id'])
+    i += 1
