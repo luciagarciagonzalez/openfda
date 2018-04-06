5e04595bde5c505c747f3868d81f0473b30a4455
l.garciago.2017 <lucia_garcia_gonzalez@hotmail.es>
2018-04-06 15:52:50 +0200

    Practice 1

--- openfda-1/show_respos_2.py
+++ openfda-1/show_respos_2.py
@@ -4,13 +4,14 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json", None, headers)
+conn.request("GET", "/drug/label.json?limit=10", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
 conn.close()
 
 repos = json.loads(repos_raw)
-print("The id is", repos['results'][0]['id'])
-print("The purpose is", repos['results'][0]['purpose'])
-print("The name of the manufacturer is", repos['results'][0]['openfda']['manufacturer_name'])
+i = 0
+while i < 10:
+    print("The id is", repos['results'][i]['id'])
+    i += 1
