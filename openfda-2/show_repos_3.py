f00bf0acb17ec2f7c3f101b97735f5644a3c4875
l.garciago.2017 <lucia_garcia_gonzalez@hotmail.es>
2018-04-06 16:33:56 +0200

    Practice 2

---
+++ openfda-2/show_repos_3.py
@@ -0,0 +1,16 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?search=active_ingredient:acetylsalicylic&limit=100", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+for i in range(len(repos['results'])):
+    if repos['results'][i]['openfda'] != {}:
+        print("The name of all manufacturers which produce aspirins is", repos['results'][i]['openfda']['manufacturer_name'])
